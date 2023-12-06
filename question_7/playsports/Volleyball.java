c679d7656987d06f4099abf2231cae2a9d5b10c7
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-12-05 17:48:50 -0800

    class 'Volleyball', implements the interface 'Playable', providing its specific implementation of the 'play' method for playing volleyball
    class 'Volleyball', implements the interface 'Playable', providing its specific implementation of the 'play' method for playing volleyball

---
+++ question_7/playsports/Volleyball.java
@@ -0,0 +1,12 @@
+package question_7.playsports;
+
+/* class 'Volleyball', implements the interface 'Playable', providing its specific implementation of the 'play' method for playing volleyball */
+class Volleyball implements Playable {
+    // method 'play' overriding the 'play' method in interface 'Playable,' for class 'Volleyball'
+    @Override
+    public void play() {
+        System.out.println("Playing Volleyball");
+        
+    }// end method 'play'
+    
+}// end class 'Volleyball'
