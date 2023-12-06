043b713bed3a20ee42cebd2e045d34758d4b10a2
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-12-05 16:53:09 -0800

    abstract class 'Shape', defining methods for calculating the area and perimeter which will be implemented by its subclasses for specific shapes
    abstract class 'Shape', defining methods for calculating the area and perimeter which will be implemented by its subclasses for specific shapes

--- question_6/shapeareaperimeter/Shape.java
+++ question_6/shapeareaperimeter/Shape.java
@@ -1,1 +1,13 @@
 package question_6.shapeareaperimeter;
+
+
+/* abstract class 'Shape', defining methods for calculating the area and perimeter 
+   which will be implemented by its subclasses for specific shapes */
+abstract class Shape {
+    // abstract method to calculate area (to be implemented by subclasses)
+    public abstract double calculateArea();
+    
+    // abstract method to calculate perimeter (to be implemented by subclasses)
+    public abstract double calculatePerimeter();
+    
+}// end abstract class 'Shape'
