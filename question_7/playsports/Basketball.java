77417303da3107e9e40717ff6735a81a021ca4a4
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-12-05 17:17:27 -0800

    Class 'Basketball', implements the interface 'Playable', defining the 'play()' method specific to basketball
    Class 'Basketball', implements the interface 'Playable', defining the 'play()' method specific to basketball

---
+++ question_7/playsports/Basketball.java
@@ -0,0 +1,12 @@
+package question_7.playsports;
+
+/* Class 'Basketball', implements the interface 'Playable', defining the 'play()' method specific to basketball */
+class Basketball implements Playable {
+    // method 'play' overriding the 'play' method in interface 'Playable', for class 'Basketball'
+    @Override
+    public void play() {
+        System.out.println("Playing Basketball");
+       
+    }// end method 'play'
+    
+}// end class 'Basketball'
