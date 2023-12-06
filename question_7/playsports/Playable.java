081d2344e9c1520c14d3c60675b08889fb87ee57
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-12-05 17:14:39 -0800

    interface 'Playable' that defines a method 'play()' to represent the action of playing a sport for entities/classes that implement this interface
    interface 'Playable' that defines a method 'play()' to represent the action of playing a sport for entities/classes that implement this interface

--- question_7/playsports/Playable.java
+++ question_7/playsports/Playable.java
@@ -1,1 +1,9 @@
 package question_7.playsports;
+
+/* interface 'Playable' that defines a method 'play()' 
+  to represent the action of playing a sport for entities/classes that implement this interface */
+interface Playable {
+    // method 'play' to play a sport
+    void play();
+    
+}// end interface 'Playable'
